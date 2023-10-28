54bb1bee10916dfe0d3fae82e8bf3236689ca953
renzo <renzobocanegra.a@gmail.com>
2023-10-28 13:06:05 -0500

    feat: add getUser method

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/database/database.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUser(id: Prisma.UserWhereUniqueInput): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: id,
+    });
+
+    return user;
+  }
+}
