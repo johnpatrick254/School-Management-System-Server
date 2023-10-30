966fb032cbe2ae5ae283e3e4df324e5e29c1fcee
renzo <renzobocanegra.a@gmail.com>
2023-10-30 15:09:14 -0500

    feat: add user crud

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,58 +1,110 @@
-import { ConflictException, HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/database.service';
 import { CreateUserDTO } from './DTO/create-user.dto';
-
+import { updateUserDTO } from './DTO/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-  async getUser(id: Prisma.UserWhereUniqueInput): Promise<User> {
+  async getUserById(id: string): Promise<User> {
     const user = await this.prisma.user.findUnique({
-      where: id,
+      where: { id },
     });
 
     if (!user) throw new NotFoundException();
     return user;
   }
+
+  async getAllUsers(): Promise<User[]> {
+    const users = [];
+    const completeUsers = await this.prisma.user.findMany();
+
+    completeUsers.map((user) =>
+      users.push({
+        name: user.name,
+        surname: user.surname,
+        roleId: user.roleId,
+        id: user.id,
+        email: user.email,
+      }),
+    );
+
+    return users;
+  }
+
   async createUser(user: CreateUserDTO): Promise<User> {
     if (user.password !== user.password_confirm) {
-      throw new HttpException("Passwords do not match", HttpStatus.UNPROCESSABLE_ENTITY);
+      throw new HttpException(
+        'Passwords do not match',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
     }
     try {
-      const { password_confirm, ...userData } = user
-      const newUser = await this.prisma.user.create({ data: userData })
-      return newUser
-
+      const { password_confirm, ...userData } = user;
+      const newUser = await this.prisma.user.create({ data: userData });
+      return newUser;
     } catch (error) {
       console.log(error.message);
       if (error.code === 'P2002') {
-        throw new ConflictException("User Already Exists")
+        throw new ConflictException('User Already Exists');
       }
-      throw new InternalServerErrorException()
-
+      throw new InternalServerErrorException();
     }
   }
-  async updateUser(id: string, user: User): Promise<User> {
+  async updateUser(id: string, user: updateUserDTO): Promise<User> {
     try {
-      const newUser = await this.prisma.user.update({ data: user, where: { id } })
-      return newUser
-
+      const newUser = await this.prisma.user.update({
+        data: user,
+        where: { id },
+      });
+      return newUser;
     } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException("User does'nt exist");
+      }
       console.log(error);
+      throw new InternalServerErrorException();
     }
   }
+
+  async updateStaffUser(
+    id: string,
+    data: Pick<updateUserDTO, 'password'>,
+  ): Promise<User> {
+    try {
+      const newUser = await this.prisma.user.update({
+        data,
+        where: { id },
+      });
+      return newUser;
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException("User does'nt exist");
+      }
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
+
   async deleteUser(id: string): Promise<User> {
     try {
-      const deletedUser = await this.prisma.user.delete({ where: { id } })
-      if (!deletedUser) {
-        throw new NotFoundException("User does'nt exist")
-      }
-      return deletedUser
+      const deletedUser = await this.prisma.user.delete({ where: { id } });
+      return deletedUser;
     } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException("User does'nt exist");
+      }
       console.log(error);
+      throw new InternalServerErrorException();
     }
   }
-
 }
