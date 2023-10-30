3c81dd9433f75bfff09bbde2384a03023d5a58b4
John Patrick <jpattrick538@gmail.com>
2023-10-30 21:05:28 +0300

    feat: add create user method

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Put, UnauthorizedException, Post, HttpStatus, HttpException, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './DTO/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('user')
+export class UsersController {
+    constructor(private userService: UsersService) { }
+
+    @Post('/')
+    async createUser(@Body() user: CreateUserDTO) {
+       return await this.userService.createUser(user)
+    }
+}
+
+
