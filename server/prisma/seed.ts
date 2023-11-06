5aee5e215e6952b63bc070459f52f96e4e773caf
John Patrick <jpattrick538@gmail.com>
2023-11-07 00:04:18 +0300

    feat:add seed data

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -5,9 +5,9 @@ const prisma = new PrismaClient()
 //Permission//
 //////////////
 const seed = async () =>{
-
+    //   prisma.$queryRaw``
     const permissions = ['EDIT_STUDENT','VIEW_STUDENT','CREATE_STUDENT','DELETE_STUDENT']
-    const savedPermissions = []
+    const savedPermissions:Permission[] = []
     permissions.map(async (permission:PermissionType) => {
         const createdPermission =  await prisma.permission.create({ data: { type:permission } })
         savedPermissions.push(createdPermission)
@@ -27,4 +27,60 @@ const seed = async () =>{
             create:savedPermissions
         } 
     }})
+///////////
+//TEACHER//
+/////////
+const newTeacher =  await prisma.user.create({data:{
+    name:"teacher ",
+    surname:"1",
+    email:"teacher-12@gmail.com",
+    password:"1234",
+    type:"TEACHER",
+    permissions:{
+        create:savedPermissions
+    } 
+}})
+const teacher = await prisma.teacher.create({
+    data:{
+        code:"WEB-TS/01",    
+        user:{
+            create:newTeacher
+        } 
+    }
+})
+
+///////////
+//Career//
+/////////
+const career = await prisma.career.create({
+    data:{
+        code:"JS-101",
+        name:"JS Fundamentals",
+        cost:10000, 
+    }
+})
+
+///////////
+//Cohort//
+/////////
+const cohort = await prisma.cohort.create({
+    data:{
+        code:"JS-2023",
+        name:"JS Fundamentals",
+        careerId:career.id
+    }
+})
+
+////////////
+//SECTION//
+//////////
+const section = await prisma.section.create({
+    data:{
+        name:"JS Fundamentals",
+        teacherId:teacher.id,
+        cohortId:cohort.id
+    }
+})
+
 }
+seed()
