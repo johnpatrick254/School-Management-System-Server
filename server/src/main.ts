d46ed2c0d09478a8db019c3743944657105088cd
John Patrick <jpattrick538@gmail.com>
2023-11-07 00:06:01 +0300

    feat:update imports

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './core/allExceptionFilter.filter';
-import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './lib/allExceptionFilter.filter';
+import { ValidationPipe } from '
+@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
