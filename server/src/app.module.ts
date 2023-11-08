d215d213a4ce2c5fc35e29cff0dbbae019055a63
John Patrick <jpattrick538@gmail.com>
2023-11-08 23:21:54 +0300

    feat:import register module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import {ConfigModule}  from "@nestjs/config"
+import { RegisterModule } from './register/register.module';
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath:`.env`}),DatabaseModule, UsersModule],
+  imports: [ConfigModule.forRoot({envFilePath:`.env`}),DatabaseModule, UsersModule,RegisterModule],
   providers: [],
 })
 export class AppModule {}
