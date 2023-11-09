54a586490bbb061a48d9166e89d70e193955c387
renzo <renzobocanegra.a@gmail.com>
2023-11-09 12:55:05 -0500

    feat: add login

--- server/src/register/register.controller.ts
+++ server/src/register/register.controller.ts
@@ -2,21 +2,46 @@ import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { RegisterService } from './register.service';
 import { CreateUserDTO } from 'src/users/DTO/create-user.dto';
 import { CreateStudentDTO } from './DTO/createstudent.dto';
-import { CreateAdminDTO } from './DTO/creatstaff.dto';
+import {
+  CreateAccountantDTO,
+  CreateAdminDTO,
+  CreateTeacherDTO,
+} from './DTO/createstaff.dto';
 
-const logger = new Logger()
+const logger = new Logger();
 @Controller('register')
 export class RegisterController {
-    constructor(private registerService: RegisterService) { }
+  constructor(private registerService: RegisterService) {}
 
-    @Post('student')
-    async registerStudent(@Body('userData') userData: CreateUserDTO, @Body('schoolData')  schoolData: CreateStudentDTO) {
-        return await this.registerService.createStudent(userData, schoolData)
-    }
-    @Post('admin')
-    async registerAdmin(@Body('userData') userData: CreateUserDTO, @Body('schoolData')  schoolData: CreateAdminDTO) {
-        return await this.registerService.createAdmin(userData, schoolData)
-    }
+  @Post('student')
+  async registerStudent(
+    @Body('userData') userData: CreateUserDTO,
+    @Body('schoolData') schoolData: CreateStudentDTO,
+  ) {
+    return await this.registerService.createStudent(userData, schoolData);
+  }
 
- 
+  @Post('teacher')
+  async registerTeacher(
+    @Body('userData') userData: CreateUserDTO,
+    @Body('schoolData') schoolData: CreateTeacherDTO,
+  ) {
+    return await this.registerService.createTeacher(userData, schoolData);
+  }
+
+  @Post('admin')
+  async registerAdmin(
+    @Body('userData') userData: CreateUserDTO,
+    @Body('schoolData') schoolData: CreateAdminDTO,
+  ) {
+    return await this.registerService.createAdmin(userData, schoolData);
+  }
+
+  @Post('accountant')
+  async registerAccountant(
+    @Body('userData') userData: CreateUserDTO,
+    @Body('schoolData') schoolData: CreateAccountantDTO,
+  ) {
+    return await this.registerService.createAccountant(userData, schoolData);
+  }
 }
--- server/src/register/register.service.ts
+++ server/src/register/register.service.ts
@@ -1,76 +1,134 @@
-import { ConflictException, HttpException, HttpStatus, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreateStudentDTO } from './DTO/createstudent.dto';
 import { CreateUserDTO } from 'src/users/DTO/create-user.dto';
 import { PrismaService } from 'src/database/database.service';
-import { CreateAdminDTO } from './DTO/creatstaff.dto';
-import { hash } from "bcrypt";
+import {
+  CreateAccountantDTO,
+  CreateAdminDTO,
+  CreateTeacherDTO,
+} from './DTO/createstaff.dto';
+import { hash } from 'bcrypt';
+import { logger } from '../lib/logger';
 
-const logger = new Logger()
 @Injectable()
 export class RegisterService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async createStudent(userData: CreateUserDTO, schoolData: CreateStudentDTO) {
-        const newPwd = crypto.randomUUID();
-        try {
-            const newStudent = await this.prisma.student.create({
-                data: {
-                    code: schoolData.code,
-                    EOC: schoolData.EOC,
-                    user: {
-                        create: {
-                            ...userData,
-                            ['password']: await hash(newPwd, 10)
-                        }
-                    },
-                    cohort: {
-                        connect: {
-                            id: schoolData.cohortId
-                        }
-                    },
-                    section: {
-                        connect: {
-                            id: schoolData.sectionId,
-                        }
-                    }
-                }
-            })
-            return { ...newStudent, newPwd };
-        } catch (error) {
-            logger.debug(error)
-            if (error.code === 'P2002') {
-                throw new ConflictException('Student Already Exists');
-            }
-            throw new InternalServerErrorException();
-        }
+  async createStudent(userData: CreateUserDTO, schoolData: CreateStudentDTO) {
+    try {
+      const newPwd = crypto.randomUUID();
+      const newStudent = await this.prisma.student.create({
+        data: {
+          code: schoolData.code,
+          EOC: schoolData.EOC,
+          user: {
+            create: {
+              ...userData,
+              ['password']: await hash(newPwd, 10),
+            },
+          },
+          cohort: {
+            connect: {
+              id: schoolData.cohortId,
+            },
+          },
+          section: {
+            connect: {
+              id: schoolData.sectionId,
+            },
+          },
+        },
+      });
+      return { ...newStudent, newPwd };
+    } catch (error) {
+      logger.debug(error);
+      if (error.code === 'P2002') {
+        throw new ConflictException('Student Already Exists');
+      }
+      throw new InternalServerErrorException();
     }
+  }
 
-    async createAdmin(userData: CreateUserDTO, schoolData: CreateAdminDTO) {
-        const newPwd = crypto.randomUUID();
-        try {
-            const admin = await this.prisma.admin.create({
-                data: {
-                    ...schoolData,
-                    user: {
-                        create: {
-                            ...userData,
-                            ['password']: await hash(newPwd, 10)
-                        }
-                    }
-                }
-            })
-
-            return {
-                ...admin,
-                newPwd
-            }
-        } catch (error) {
-            logger.debug(error)
-            if (error.code === 'P2002') {
-                throw new ConflictException('Admin Already Exists');
-            }
-            throw new InternalServerErrorException();
-        }
-
+  async createTeacher(userData: CreateUserDTO, schoolData: CreateTeacherDTO) {
+    try {
+      const newPwd = crypto.randomUUID();
+      const newTeacher = await this.prisma.teacher.create({
+        data: {
+          code: schoolData.code,
+          user: {
+            create: {
+              ...userData,
+              ['password']: await hash(newPwd, 10),
+            },
+          },
+        },
+      });
+      return { ...newTeacher, newPwd };
+    } catch (error) {
+      logger.debug(error);
+      if (error.code === 'P2002') {
+        throw new ConflictException('Teacher Already Exists');
+      }
+      throw new InternalServerErrorException();
     }
+  }
+
+  async createAdmin(userData: CreateUserDTO, schoolData: CreateAdminDTO) {
+    try {
+      const newPwd = crypto.randomUUID();
+      const admin = await this.prisma.admin.create({
+        data: {
+          ...schoolData,
+          user: {
+            create: {
+              ...userData,
+              ['password']: await hash(newPwd, 10),
+            },
+          },
+        },
+      });
+
+      return {
+        ...admin,
+        newPwd,
+      };
+    } catch (error) {
+      logger.debug(error);
+      if (error.code === 'P2002') {
+        throw new ConflictException('Admin Already Exists');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async createAccountant(
+    userData: CreateUserDTO,
+    schoolData: CreateAccountantDTO,
+  ) {
+    try {
+      const newPwd = crypto.randomUUID();
+      const accountant = await this.prisma.accountant.create({
+        data: {
+          ...schoolData,
+          user: {
+            create: {
+              ...userData,
+              ['password']: await hash(newPwd, 10),
+            },
+          },
+        },
+      });
+      return { ...accountant, newPwd };
+    } catch (error) {
+      logger.debug(error);
+      if (error.code === 'P2002') {
+        throw new ConflictException('Accountant Already Exists');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
 }
