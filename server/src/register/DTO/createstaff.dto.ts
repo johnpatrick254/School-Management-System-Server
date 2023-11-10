7c5d75249155e6d915c58990a0d064e446959c12
renzo <renzobocanegra.a@gmail.com>
2023-11-10 13:40:12 -0500

    fix: update fields after remove user model

--- server/src/register/DTO/createstaff.dto.ts
+++ server/src/register/DTO/createstaff.dto.ts
@@ -1,30 +1,55 @@
-import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
-
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTeacherDTO {
+  @IsNotEmpty()
+  @IsString()
+  code: string;
 
+  @IsNotEmpty()
+  @IsString()
+  name: string;
 
-    @IsNotEmpty()
-    @IsString()
-    code: string
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
 
-
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
 
+export class CreateAdminDTO {
+  @IsNotEmpty()
+  @IsString()
+  code: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+}
 
 export class CreateAccountantDTO {
+  @IsNotEmpty()
+  @IsString()
+  code: string;
 
-    @IsNotEmpty()
-    @IsString()
-    code: string
+  @IsNotEmpty()
+  @IsString()
+  name: string;
 
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
 
-}
-export class CreateAdminDTO {
-
-    @IsNotEmpty()
-    @IsString()
-    code: string
-
-
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
--- server/src/register/DTO/createstudent.dto.ts
+++ server/src/register/DTO/createstudent.dto.ts
@@ -1,17 +1,31 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateStudentDTO {
+  @IsNotEmpty()
+  @IsString()
+  code: string;
 
-    @IsNotEmpty()
-    @IsString()
-    code: string
-    @IsNotEmpty()
-    @IsString()
-    EOC: string
-    @IsNotEmpty()
-    @IsString()
-    cohortId: string
-    @IsNotEmpty()
-    @IsString()
-    sectionId: string
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  surname: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  EOC: string;
+
+  @IsNotEmpty()
+  @IsString()
+  cohortId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  sectionId: string;
 }
