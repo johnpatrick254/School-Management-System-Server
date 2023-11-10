baf648d4fe76d1b511bd940a3e0e2fb86b0d493f
John Patrick <jpattrick538@gmail.com>
2023-11-11 00:28:16 +0300

    feat:export auth service

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, ConfigService],
+  providers: [AuthService, ConfigService, AuthGuard],
   imports: [DatabaseModule],
+  exports: [AuthService],
 })
 export class AuthModule {}
