e6397b6924d260a79f7718a8dd35476962f6c74c
John Patrick <jpattrick538@gmail.com>
2023-11-11 00:27:52 +0300

    feat:add validate method

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { loginDTO } from './DTO/login.dto';
 import { AuthService } from './auth.service';
 import * as Jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { Public } from './publicroute.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -12,6 +21,7 @@ export class AuthController {
     private readonly config: ConfigService,
   ) {}
 
+  @Public()
   @Post('login/student')
   async loginStudent(
     @Body() data: loginDTO,
@@ -25,9 +35,27 @@ export class AuthController {
     const cookie = Jwt.sign(currentStudent, secret, {
       expiresIn: '1h',
     });
-    res.cookie('session', cookie, { httpOnly: true, maxAge: 3600 });
+    res
+      .cookie('session', cookie, { httpOnly: true, maxAge: 3600 })
+      .send(currentStudent);
+  }
+  @Public()
+  @Post('login/staff')
+  async loginStaff(
+    @Body() data: loginDTO,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const secret = this.config.get('SECRET');
 
-    return currentStudent;
+    const { password, ...currentStudent } =
+      await this.authService.loginStaff(data);
+
+    const cookie = Jwt.sign(currentStudent, secret, {
+      expiresIn: '1h',
+    });
+    res
+      .cookie('session', cookie, { httpOnly: true, maxAge: 3600 })
+      .send(currentStudent);
   }
 
   @Get('logout')
@@ -36,5 +64,13 @@ export class AuthController {
       .cookie('session', { httpOnly: true, maxAge: 0 })
       .status(200)
       .send({ message: 'Logged out successfully' });
+  }
+
+  @Get('validate')
+  async validateToken(@Req() req: Request, @Res() res: Response) {
+    const accessToken = req.cookies['session'];
+    if (!accessToken) throw new UnauthorizedException();
+    const isValid = this.authService.validateUser(accessToken);
+    if (isValid === true) res.status(200);
   }
 }
