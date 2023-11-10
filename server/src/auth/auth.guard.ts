b88dde0ef9d2ebb139d38d7c2ba1c67b128a6caf
John Patrick <jpattrick538@gmail.com>
2023-11-11 00:25:18 +0300

    feat:setup authguard

--- server/src/auth/auth.guard.ts
+++ server/src/auth/auth.guard.ts
@@ -1,11 +1,40 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './publicroute.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private reflector: Reflector,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    const req: Request = context.switchToHttp().getRequest();
+
+    const token = req.cookies['session'];
+
+    if (!token) throw new UnauthorizedException();
+
+    this.authService.validateUser(token);
+
     return true;
   }
 }
