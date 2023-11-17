ad2c5c4c694261ae3bac964ab0b1d8cf149b9112
John Patrick <jpattrick538@gmail.com>
2023-11-17 20:35:37 +0300

    feat: add permision and login guard gates

--- server/src/auth/auth.guard.ts
+++ server/src/auth/auth.guard.ts
@@ -9,13 +9,18 @@ import { AuthService } from './auth.service';
 import { Request } from 'express';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from './publicroute.decorator';
+import { extractBearerToken } from './Util/extracttoken.util';
+import { RequiredPermission } from 'src/auth/permision.decorator';
+import { PrismaService } from 'src/database/database.service';
+import { PermissionType } from '@prisma/client';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly authService: AuthService,
     private reflector: Reflector,
-  ) {}
+    private readonly prisma: PrismaService
+  ) { }
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -23,17 +28,23 @@ export class AuthGuard implements CanActivate {
       context.getHandler(),
       context.getClass(),
     ]);
-    if (isPublic) {
-      return true;
-    }
-
     const req: Request = context.switchToHttp().getRequest();
-
-    const token = req.cookies['session'];
-
+    const { authorization } = req.headers;
+    const requiredPermission = this.reflector.get(RequiredPermission, context.getHandler()) as Partial<PermissionType>
+    const token = extractBearerToken(authorization);
     if (!token) throw new UnauthorizedException();
 
+    if (isPublic) {
+      if (requiredPermission) {
+        return this.authService.validateUserPerms(requiredPermission,token);  
+      }
+      return true;
+    }
+   
     this.authService.validateUser(token);
+    if (requiredPermission) {
+      return this.authService.validateUserPerms(requiredPermission,token);  
+    }
 
     return true;
   }
