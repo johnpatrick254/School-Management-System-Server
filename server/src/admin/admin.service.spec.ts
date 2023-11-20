6ae6f384d24152fb0f6ed467f54c788689867c49
John Patrick <jpattrick538@gmail.com>
2023-11-20 20:54:30 +0300

    feat: add unit test for update and update password methods

--- server/src/admin/admin.service.spec.ts
+++ server/src/admin/admin.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
 import { PrismaService } from '../database/database.service';
 import { NotFoundException } from '@nestjs/common';
+import { UpdateAdminDTO } from './DTO/update-admin.dto';
+import { UpdatePasswordDTO } from 'src/core/DTO/update-password.dto';
 
 describe('AdminService', () => {
   let service: AdminService;
@@ -36,7 +38,7 @@ describe('AdminService', () => {
             admin: {
               findUnique: jest.fn(),
               update: jest.fn(),
-              findMany:jest.fn(),
+              findMany: jest.fn(),
             },
           },
         },
@@ -48,7 +50,7 @@ describe('AdminService', () => {
   });
 
   describe("getAdminById", () => {
-    it("should be defined",()=>{
+    it("should be defined", () => {
       expect(service.getAdminById).toBeDefined();
     })
 
@@ -65,40 +67,71 @@ describe('AdminService', () => {
     });
   });
 
-  describe("getAdmins",async ()=>{
+  describe("getAdmins", async () => {
     const limit = 5;
-    it("should be defined",()=>{
+    it("should be defined", () => {
       expect(service.getAdmins).toBeDefined();
     })
 
-    it('Should returns admins without password',async ()=>{
-      jest.spyOn(prisma.admin,"findMany").mockResolvedValue([{password:"1234",...mockReturnedAdmin}])
+    it('Should returns admins without password', async () => {
+      jest.spyOn(prisma.admin, "findMany").mockResolvedValue([{ password: "1234", ...mockReturnedAdmin }])
       const admins = await service.getAdmins(limit);
       expect(admins).toEqual([mockReturnedAdmin]);
     })
 
-    it("Should return  number admins equal to limit",async ()=>{
-      jest.spyOn(prisma.admin,"findMany").mockResolvedValue([{password:"1234",...mockReturnedAdmin},{password:"1234",...mockReturnedAdmin},{password:"1234",...mockReturnedAdmin},{password:"1234",...mockReturnedAdmin},{password:"1234",...mockReturnedAdmin},])
+    it("Should return  number admins equal to limit", async () => {
+      jest.spyOn(prisma.admin, "findMany").mockResolvedValue([{ password: "1234", ...mockReturnedAdmin }, { password: "1234", ...mockReturnedAdmin }, { password: "1234", ...mockReturnedAdmin }, { password: "1234", ...mockReturnedAdmin }, { password: "1234", ...mockReturnedAdmin },])
       const admins = await service.getAdmins(limit);
       expect(admins.length).toEqual(limit);
     })
   });
 
-  describe("updateAdmin",()=>{
-    it("should be defined",()=>{
-      expect(service.updateAdmin).toBeDefined();
+  describe("update", () => {
+    it("should be defined", () => {
+      expect(service.update).toBeDefined();
+    });
+
+    it("should be be called with update-admin dto", () => {
+      const updateData = new UpdateAdminDTO()
+      jest.spyOn(prisma.admin, "update").mockResolvedValue(mockAdmin);
+      service.updateAdmin(updateData);
+      expect(service.update).toBeCalledWith(updateData);
     });
 
     it("should throw 404 if there's no admin", async () => {
       jest.spyOn(prisma.admin, "update").mockResolvedValue(null);
-      await expect(service.updateAdmin("1423",{name:"john"})).rejects.toThrow(new NotFoundException('Admin does not exist'));
+      await expect(service.update("1423", { name: "john" })).rejects.toThrow(new NotFoundException('Admin does not exist'));
     });
 
     it('should return admin without password after update', async () => {
       jest.spyOn(prisma.admin, "update").mockResolvedValue({ password: "123", ...mockReturnedAdmin });
-      const admin = await service.updateAdmin("1423",{name:"john"});
+      const admin = await service.update("1423", { name: "john" });
       expect(admin).toEqual(mockReturnedAdmin);
     });
   })
-  
+
+  describe("updatePassword", () => {
+    it('should be defined', () => {
+      expect(service.updatePassword).toBeDefined();
+    });
+
+    it("should be called with update-password-dto", async () => {
+      const password = new UpdatePasswordDTO()
+      jest.spyOn(prisma.admin, "update").mockResolvedValue(mockAdmin);
+      await service.updatePassword(password);
+      expect(service.updatePassword).toBeCalledWith(password);
+    });
+
+    it("should return null", async () => {
+      const password = new UpdatePasswordDTO()
+      jest.spyOn(prisma.admin, "update").mockResolvedValue(mockAdmin);
+      expect(await service.updatePassword(password)).toReturnWith(null);
+    });
+    it("should throw 404 if there's no admin", async () => {
+      const password = new UpdatePasswordDTO()
+      jest.spyOn(prisma.admin, "update").mockResolvedValue(null);
+      await expect(service.updatePassword(password)).rejects.toThrow(new NotFoundException('Admin does not exist'));
+    });
+  })
+
 });
