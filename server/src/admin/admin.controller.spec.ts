1b95aa5ccc566d1443d6102bfabdf670db3c7952
renzo <renzobocanegra.a@gmail.com>
2023-11-23 14:07:24 -0500

    fix: update module init and add delete unit test

--- server/src/admin/admin.controller.spec.ts
+++ server/src/admin/admin.controller.spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminController } from './admin.controller';
 import { UpdateAdminDTO } from './DTO/update-admin.dto';
-import { UpdatePasswordDTO } from 'src/core/DTO/update-password.dto';
+import { UpdatePasswordDTO } from '../core/DTO/update-password.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { PrismaService } from '../database/database.service';
 
 describe('AdminController', () => {
   let controller: AdminController;
@@ -16,6 +19,20 @@ describe('AdminController', () => {
   };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AdminService,
+        {
+          provide: PrismaService,
+          useValue: {
+            admin: {
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              findMany: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
       controllers: [AdminController],
     }).compile();
 
@@ -26,40 +43,57 @@ describe('AdminController', () => {
     expect(controller).toBeDefined();
   });
 
-  describe("getAdminById", () => {
+  describe('getAdminById', () => {
     it('should be defined', () => {
       expect(controller.getAdminById).toBeDefined();
     });
   });
 
-  describe("getAdmins", () => {
+  describe('getAdmins', () => {
     it('should be defined', () => {
       expect(controller.getAdmins).toBeDefined();
     });
+    it('should throw httpException when not limit is provided', async () => {
+      jest.spyOn(controller, 'getAdmins');
+      await expect(controller.getAdmins(null)).rejects.toThrow(
+        new HttpException('No limit provided', HttpStatus.UNPROCESSABLE_ENTITY),
+      );
+    });
   });
 
-  describe("update", () => {
+  describe('update', () => {
     it('should be defined', () => {
       expect(controller.update).toBeDefined();
     });
-    it("should be called with updateAdminDTO", async () => {
-      const updateAdminDTO = new UpdateAdminDTO()
-      jest.spyOn(controller, "update").mockResolvedValue(mockReturnedAdmin)
-      await controller.update(updateAdminDTO);
-      expect(controller.update).toBeCalledWith(updateAdminDTO);
+    it('should be called with updateAdminDTO', async () => {
+      const { id, ...adminData } = new UpdateAdminDTO();
+      jest.spyOn(controller, 'update').mockResolvedValue(mockReturnedAdmin);
+      await controller.update(id, adminData);
+      expect(controller.update).toBeCalledWith(id, adminData);
+    });
+  });
 
-    })
-  })
-
-  describe("updatePassword", () => {
+  describe('updatePassword', () => {
     it('should be defined', () => {
       expect(controller.updatePassword).toBeDefined();
     });
-    it("should be called with updatePasswordDTO", async () => {
-      const updatePasswordDTO = new UpdatePasswordDTO()
-      jest.spyOn(controller, "updatePassword").mockResolvedValue(mockReturnedAdmin)
-      await controller.update(updatePasswordDTO);
+    it('should be called with updatePasswordDTO', async () => {
+      const updatePasswordDTO = new UpdatePasswordDTO();
+      jest.spyOn(controller, 'updatePassword').mockResolvedValue(null);
+      await controller.updatePassword(updatePasswordDTO);
       expect(controller.updatePassword).toBeCalledWith(updatePasswordDTO);
-    })
+    });
   });
-})
+
+  describe('delete', () => {
+    it('should be defined', () => {
+      expect(controller.delete).toBeDefined();
+    });
+    it('should be call with string id', async () => {
+      const id: string = '123';
+      jest.spyOn(controller, 'delete').mockResolvedValue(null);
+      await controller.delete(id);
+      expect(controller.delete).toBeCalledWith(id);
+    });
+  });
+});
