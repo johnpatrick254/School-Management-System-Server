574c66b7277454a6def44a823c4bde9e57570320
John Patrick <jpattrick538@gmail.com>
2023-11-30 22:03:54 +0300

    feat:implement CRUD methods

--- server/src/cohort/cohort.service.ts
+++ server/src/cohort/cohort.service.ts
@@ -1,7 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/database/database.service';
+import { Body, ConflictException, Get, Injectable, InternalServerErrorException, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { PrismaService } from '../database/database.service';
+import { CreateCohortDTO } from './DTO/createcohort.dto';
+import { UpdateCohortDTO } from './DTO/updatecohort.dto';
+import { Cohort } from '@prisma/client';
 
 @Injectable()
 export class CohortService {
-  constructor(readonly prisma: PrismaService) {}
+  constructor(readonly prisma: PrismaService) { }
+
+  @Post()
+  async create(data: CreateCohortDTO): Promise<Cohort> {
+    try {
+      const cohort = await this.prisma.cohort.create({
+        data
+      });
+      return cohort;
+    } catch (error) {
+      if (error.code === "P2002") throw new ConflictException('cohort already exist');
+      throw new InternalServerErrorException()
+    }
+  };
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: Pick<UpdateCohortDTO, "careerId" | "code" | "name">) {
+    const updatedCohort = await this.prisma.cohort.update({
+      data,
+      where: { id }
+    })
+
+    if (!updatedCohort) throw new NotFoundException('cohort not found');
+
+    return updatedCohort;
+  };
+
+  @Get(':id')
+  async getCohortById(@Param('id') id: string) {
+    const cohort = await this.prisma.cohort.findUnique({
+      where: { id }
+    });
+    if (!cohort) throw new NotFoundException('cohort not found');
+    return cohort;
+  }
+
+  @Get()
+  async getCohorts(limit: number): Promise<Cohort[]> {
+    const cohorts = await this.prisma.cohort.findMany({
+      take: limit
+    })
+
+    return cohorts;
+  }
+
+  async delete(@Param('id') id: string) {
+
+    const deleted = await this.prisma.cohort.delete({ where: { id } });
+    if (!deleted) throw new NotFoundException('cohort not found');
+  }
 }
