0c8a409410bc2bbf49ac71b45bff67fc783314a9
renzo <renzobocanegra.a@gmail.com>
2023-11-30 15:05:27 -0500

    fix: remove decorators

--- server/src/cohort/cohort.service.ts
+++ server/src/cohort/cohort.service.ts
@@ -1,4 +1,14 @@
-import { Body, ConflictException, Get, Injectable, InternalServerErrorException, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Get,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PrismaService } from '../database/database.service';
 import { CreateCohortDTO } from './DTO/createcohort.dto';
 import { UpdateCohortDTO } from './DTO/updatecohort.dto';
@@ -6,53 +16,52 @@ import { Cohort } from '@prisma/client';
 
 @Injectable()
 export class CohortService {
-  constructor(readonly prisma: PrismaService) { }
+  constructor(readonly prisma: PrismaService) {}
 
-  @Post()
   async create(data: CreateCohortDTO): Promise<Cohort> {
     try {
       const cohort = await this.prisma.cohort.create({
-        data
+        data,
       });
       return cohort;
     } catch (error) {
-      if (error.code === "P2002") throw new ConflictException('cohort already exist');
-      throw new InternalServerErrorException()
+      if (error.code === 'P2002')
+        throw new ConflictException('cohort already exist');
+      throw new InternalServerErrorException();
     }
-  };
+  }
 
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() data: Pick<UpdateCohortDTO, "careerId" | "code" | "name">) {
+  async update(
+    id: string,
+    data: Pick<UpdateCohortDTO, 'careerId' | 'code' | 'name'>,
+  ) {
     const updatedCohort = await this.prisma.cohort.update({
       data,
-      where: { id }
-    })
+      where: { id },
+    });
 
     if (!updatedCohort) throw new NotFoundException('cohort not found');
 
     return updatedCohort;
-  };
+  }
 
-  @Get(':id')
-  async getCohortById(@Param('id') id: string) {
+  async getCohortById(id: string) {
     const cohort = await this.prisma.cohort.findUnique({
-      where: { id }
+      where: { id },
     });
     if (!cohort) throw new NotFoundException('cohort not found');
     return cohort;
   }
 
-  @Get()
   async getCohorts(limit: number): Promise<Cohort[]> {
     const cohorts = await this.prisma.cohort.findMany({
-      take: limit
-    })
+      take: limit,
+    });
 
     return cohorts;
   }
 
-  async delete(@Param('id') id: string) {
-
+  async delete(id: string) {
     const deleted = await this.prisma.cohort.delete({ where: { id } });
     if (!deleted) throw new NotFoundException('cohort not found');
   }
