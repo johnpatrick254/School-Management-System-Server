efc00e493a933cf4566ee9f9c93f4c3519d88d19
John Patrick <jpattrick538@gmail.com>
2023-12-22 21:04:07 +0300

    fix:add id to updateDTO

--- server/src/section/DTO/updatesection.dto.ts
+++ server/src/section/DTO/updatesection.dto.ts
@@ -1,10 +1,13 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateSectionDTO {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
   @IsOptional()
   @IsString()
   name: string;
-
   @IsOptional()
   @IsString()
   cohortId: string;
