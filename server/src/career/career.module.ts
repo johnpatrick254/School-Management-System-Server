4875904636cfad5332ed916c4c1bd21d19d34fcf
renzo <renzobocanegra.a@gmail.com>
2023-12-23 12:30:06 -0500

    fix: PrismaService provider added

--- server/src/career/career.module.ts
+++ server/src/career/career.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CareerController } from './career.controller';
 import { CareerService } from './career.service';
+import { PrismaService } from 'src/database/database.service';
 
 @Module({
   controllers: [CareerController],
-  providers: [CareerService]
+  providers: [CareerService, PrismaService],
 })
 export class CareerModule {}
