a0e7006714cd1054ad169aed2bba6e45e3f429d0
John Patrick <jpattrick538@gmail.com>
2023-12-26 16:35:45 +0300

    feat:add createCourseDTO

---
+++ server/src/course/DTO/createcourse.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCourseDTO {
+  @IsNotEmpty()
+  @IsString()
+  code: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  teacherId: string;
+}
