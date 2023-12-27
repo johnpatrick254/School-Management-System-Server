c723d025c8d0d87a0f17b5c1046b5d5c633a052f
renzo <renzobocanegra.a@gmail.com>
2023-12-27 16:27:17 -0500

    feat: course permissions added

--- server/src/course/course.controller.ts
+++ server/src/course/course.controller.ts
@@ -14,16 +14,19 @@ import { CourseService } from './course.service';
 import { CreateCourseDTO } from './DTO/createcourse.dto';
 import { Course } from '@prisma/client';
 import { UpdateCourseDTO } from './DTO/updatecourse.dto';
+import { RequiredPermission } from 'src/auth/permision.decorator';
 
 @Controller('course')
 export class CourseController {
   constructor(private service: CourseService) {}
 
+  @RequiredPermission('CREATE_COURSE')
   @Post()
   async create(@Body() data: CreateCourseDTO): Promise<Course> {
     return await this.service.create(data);
   }
 
+  @RequiredPermission('UPDATE_COURSE')
   @Put(':id')
   async update(
     @Param('id') id: string,
@@ -33,11 +36,13 @@ export class CourseController {
     return await this.service.update(id, updateData);
   }
 
+  @RequiredPermission('VIEW_COURSE')
   @Get(':id')
   async getCourseById(@Param('id') id: string): Promise<Course> {
     return await this.service.getCourseById(id);
   }
 
+  @RequiredPermission('VIEW_COURSE')
   @Get('/')
   async getCourses(@Query('limit') limit: string): Promise<Course[]> {
     if (!limit)
@@ -45,6 +50,7 @@ export class CourseController {
     return await this.service.getCourses(+limit);
   }
 
+  @RequiredPermission('DELETE_COURSE')
   @Delete(':id')
   async delete(@Param('id') id: string) {
     if (!id) throw new HttpException('No id provided', HttpStatus.BAD_REQUEST);
