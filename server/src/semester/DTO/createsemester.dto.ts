f7d8ffbe3400c9b217ad8458e5e2db1752d217c2
John Patrick <jpattrick538@gmail.com>
2023-12-27 23:52:19 +0300

    fix:remove name property from dto

--- server/src/semester/DTO/createsemester.dto.ts
+++ server/src/semester/DTO/createsemester.dto.ts
@@ -4,16 +4,9 @@ import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 export class CreateSemesterDTO {
   @IsNotEmpty()
   @IsString()
-  name: string;
-
-  @IsNotEmpty()
-  @IsString()
   academicYear: string;
 
   @IsNotEmpty()
   @IsString()
   careerId: string;
-
-  @IsArray()
-  courses: Course[];
 }
--- server/src/semester/DTO/updatesemester.dto.ts
+++ server/src/semester/DTO/updatesemester.dto.ts
@@ -4,13 +4,10 @@ import { IsArray, IsOptional, IsString } from 'class-validator';
 export class UpdateSemesterDTO {
   @IsOptional()
   @IsString()
-  name: string;
+  careerId: string;
 
   @IsOptional()
   @IsString()
   academicYear: string;
 
-  @IsOptional()
-  @IsArray()
-  courses: Course[];
 }
