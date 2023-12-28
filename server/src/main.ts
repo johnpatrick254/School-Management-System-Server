ccfc7993536ecaeddf79de872ff49e08163aa638
John Patrick <jpattrick538@gmail.com>
2023-12-28 22:34:17 +0300

    feat:enforce cors urls

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './lib/allExceptionFilter.filter';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const FRONTED_URL = configService.get('FRONTEND_URL')
   app.enableCors(
     {
-      origin:"*",
-      credentials:true
-      
+      origin:['localhost:3000',FRONTED_URL],
+      credentials:true  
     });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
