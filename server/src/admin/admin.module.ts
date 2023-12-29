194fae071dd314735741f909b588ef5faf8af55c
John Patrick <jpattrick538@gmail.com>
2023-12-29 19:17:06 +0300

    fix:change absolute paths to relative paths

--- server/src/admin/admin.module.ts
+++ server/src/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
-import { PrismaService } from 'src/database/database.service';
+import { PrismaService } from '../database/database.service';
 
 @Module({
   providers: [AdminService, PrismaService],
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
