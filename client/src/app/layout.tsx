397276e6e872e4ff0fa06831c35bab16a5a8c906
John Patrick <jpattrick538@gmail.com>
2024-01-03 17:41:08 +0300

    fix:hide scroll-bar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "antialiased min-h-screen")}>
+      <body className={cn(inter.className, "antialiased min-h-screen no-scrollbar")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
