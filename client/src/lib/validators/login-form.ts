13176c6dfdc7b4b280d8f1792fe1ba00d45b34b4
renzo <renzobocanegra.a@gmail.com>
2024-01-04 21:42:26 -0500

    fix: not empty validation added

--- client/src/lib/validators/login-form.ts
+++ client/src/lib/validators/login-form.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  code: z.string().max(10),
-  password: z.string(),
+  code: z.string().min(1).max(10),
+  password: z.string().min(1),
 });
 
 export type LoginType = z.infer<typeof loginSchema>;
