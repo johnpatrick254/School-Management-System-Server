1e6fe17f564420c2c81a16eb614f709b9d8e31d3
renzo <renzobocanegra.a@gmail.com>
2024-01-08 23:05:43 -0500

    fix: image and testimonial updated

--- client/src/app/(auth)/sign-in/page.tsx
+++ client/src/app/(auth)/sign-in/page.tsx
@@ -7,8 +7,8 @@ const page = () => {
     <div className="flex flex-row h-screen bg-background-strong">
       <div className="hidden z-10 h-full w-[55%] p-10 text-white border-r border-border relative lg:flex lg:flex-col">
         <Image
-          src="/images/director-ai.jpg"
-          alt="director-img"
+          src="/images/students-teachers-ai.jpg"
+          alt="students-teachers-img"
           className="absolute inset-0 object-cover w-full h-full object-top"
           quality={60}
           fill
@@ -16,13 +16,15 @@ const page = () => {
         <div className="z-20 mt-auto text-primary">
           <blockquote className="space-y-2 bg-background-strong p-2 rounded opacity-90">
             <span className="text-lg p-2 italic font-light">
-              &ldquo;As a school director, implementing the Quantum school
-              management app has revolutionized the way we operate. The seamless
-              integration of Quantum into our daily processes has significantly
-              improved efficiency and communication across the board&rdquo;
+              &ldquo;Quantum has revolutionized my approach to classroom
+              management. This user-friendly school management system ensures
+              seamless communication with students and provides valuable
+              insights into their performance. Quantum has truly elevated my
+              teaching experience, making administrative tasks efficient and
+              allowing me to focus more on what matters most - teaching.&rdquo;
             </span>
             <footer className="text-sm font-bold text-tertiary">
-              Jonathan Mitchell, Director of The Coolest Awesome School.
+              Ms. Anderson, Teacher of The Coolest Awesome School.
             </footer>
           </blockquote>
         </div>
