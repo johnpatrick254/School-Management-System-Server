54da871d56d3c7035bdef7c5c76bf565fd0e19bd
John Patrick <jpattrick538@gmail.com>
2024-01-10 22:18:36 +0300

    feat: add features constants

--- client/src/components/landing-page/constants/index.ts
+++ client/src/components/landing-page/constants/index.ts
@@ -1,5 +1,9 @@
 import { Github, Linkedin } from "lucide-react";
 import { FeatureCardProps } from "../FeatureCard";
+import insightIMG from "../../../../public/images/insights-svg.svg"
+import staffIMG from "../../../../public/images/staff-svg.svg"
+import studentIMG from "../../../../public/images/students-svg.svg"
+import courseMG from "../../../../public/images/courses-svg.svg"
 
 export const navLinks = [
   {
@@ -19,14 +23,32 @@ export const navLinks = [
     path: "#team",
   },
 ];
-export const Features:FeatureCardProps[]=[
+export const features: Pick<FeatureCardProps,"bulletin" | "desc" | "title" | "img">[] = [
   {
-    title:'Manage Staff Seamlessly',
-    bulletin:"Staff Management",
-    desc:"",
-    img:""
-  }
-]
+    title: 'Get Real Time Insights',
+    bulletin: 'Real Time Insights',
+    desc: 'Access instant, comprehensive data analysis and reports for timely decision-making and actionable insights.',
+    img: insightIMG,
+  }, {
+    title: 'Manage Staff Seamlessly',
+    bulletin: 'Staff Management',
+    desc: 'Effortlessly handle staff operations, schedules, and tasks for streamlined workflow and efficient team management.',
+    img: staffIMG,
+  },
+  {
+    title: 'Manage Students Effortlessly',
+    bulletin: 'Student Management',
+    desc: 'Effortlessly oversee student records, courses, and progress for a smoother academic administrative process.',
+    img: studentIMG,
+  },
+  {
+    title: 'Organize your Academic Careers',
+    bulletin: 'Career Management',
+    desc: 'Efficiently structure and manage academic pathways, courses, and milestones for successful career development.',
+    img: courseMG,
+  },
+
+];
 export const teamMembers = [
   {
     name: "John P",
