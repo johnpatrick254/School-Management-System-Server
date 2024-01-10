0506f6177de7626c55ae17d6bd912c715d10565b
John Patrick <jpattrick538@gmail.com>
2024-01-10 22:14:16 +0300

    feat: add team section

--- client/src/components/landing-page/TeamSection.tsx
+++ client/src/components/landing-page/TeamSection.tsx
@@ -1,20 +1,80 @@
+import Image from "next/image";
 import { FC } from "react";
+import { teamMembers } from "./constants";
+import TransitionContainer from "../shared/TransitionContainer";
 
-interface TeamSectionProps {}
+interface TeamSectionProps { }
 
-const TeamSection: FC<TeamSectionProps> = ({}) => {
+const TeamSection: FC<TeamSectionProps> = ({ }) => {
   return (
-    <div id="team" className="h-screen pt-12 relative">
-      TeamSection
-      {/* close sections wave - transition to footer */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-        className="absolute -bottom-0.5 fill-background-strong stroke-background-strong"
-      >
-        <path d="M0,256L80,261.3C160,267,320,277,480,261.3C640,245,800,203,960,181.3C1120,160,1280,160,1360,160L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-      </svg>
-    </div>
+    <TransitionContainer variant="TOP" id="team" className="min-h-max w-full py-16 px-8 flex flex-col gap-y-5 ">
+
+      <h1 className="text-tertiary-foreground pb-2 text-center text-2xl font-bold rounded-sm">Team</h1>
+
+      <ul className={`
+      flex gap-10 flex-col justify-center mt-8
+      lg:flex-row
+      `}>
+        {teamMembers.map((member, i) => (
+          <li key={i} className="flex flex-col gap-3 items-center">
+
+            <div className="w-full flex justify-between gap-x-5">
+              <Image
+                src={member.photo!}
+                width={130}
+                height={130}
+                className="rounded-full h-full shadow"
+                alt={member.name!}
+              />
+              <div className="flex flex-col w-3/4 gap-y-2 justify-center "  >
+                <h2 className=" text-left text-2xl font-bold rounded-sm">{i == 0 ? 'John Onyango':"Renzo Bocanegra"}</h2>
+                <div className="text-left">
+                  <p className=" text-sm tracking-wider">
+                    {member.role}
+                  </p>
+                </div>
+                <div className="flex flex-row gap-4">
+                  {member.socialNetworks?.map((link, i) => (
+                    <a
+                      key={i}
+                      href={link.url}
+                      className="bg-primary rounded-full p-2 hover:bg-tertiary transition-colors group"
+                    >
+                      <span className="sr-only">{link.name}</span>
+                      <link.logo
+                        size={18}
+                        className="text-primary-foreground group-hover:text-white"
+                      />
+                    </a>
+                  ))}
+                </div>
+
+              </div>
+            </div>
+
+            {i == 0
+              ?
+              <>
+                <p className="text-secondary text-center">
+                  I am a dedicated and adaptable professional with a background in research biology, and a strong passion for full-stack web development, based in Nairobi, Kenya. With a profound appreciation for sleek and user-friendly designs, I bring a unique blend of skills to the tech world.
+                </p>
+              </>
+              :
+              <>
+
+                <p className="text-secondary text-center">
+                  Mechanical-Electrical Engineer specialized in Full-Stack software development whose passionate about learning new technologies and enhancing my coding skills in terms of clarity and efficiency to facilitate collaboration within other developers and optimize application performance        </p>
+
+
+              </>
+            }
+
+          </li>
+
+        ))}
+      </ul>
+     
+    </TransitionContainer>
   );
 };
 
