5eb241e81caffe1e81b415ecfb1f691b51ee44a2
John Patrick <jpattrick538@gmail.com>
2024-01-31 23:49:20 +0300

    fix: configure  cors policies

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './lib/allExceptionFilter.filter';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { logger } from './lib/logger';
 import helmet from 'helmet';
@@ -14,7 +14,7 @@ async function bootstrap() {
   const PORT = configService.get('PORT');
   app.use(helmet());
   app.enableCors({
-    origin: ['http://localhost:3000', FRONTED_URL],
+    origin: `${FRONTED_URL}`,
     credentials: true,
     allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type','Accept'],
   });
